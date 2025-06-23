8bf79289f5532f96d4ccf1ddbf181d5aef3673d0
Zigl3ur <eden.douru@hotmail.com>
2025-06-23 11:53:21 +0200

    feat: endpoint music

---
+++ src/handlers/music.ts
@@ -0,0 +1,29 @@
+import type { LastFMData } from "../types/types";
+
+export async function LastFMHandler(LASTFM_API_KEY: string) {
+  const response = await fetch(
+    `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=zigl3ur&api_key=${LASTFM_API_KEY}&format=json`
+  );
+
+  if (!response.ok) return { isListening: false };
+
+  const data: LastFMData = (await response.json()) as LastFMData;
+
+  const tracks = data.recenttracks.track;
+  const isListening =
+    tracks.length > 0 && tracks[0]["@attr"]?.nowplaying === "true";
+
+  if (isListening)
+    return {
+      isListening: isListening,
+      track: {
+        artist: tracks[0].artist["#text"],
+        album: tracks[0].album["#text"],
+        name: tracks[0].name,
+        image: tracks[0].image.find((img) => img.size === "large")?.["#text"],
+        url: tracks[0].url,
+      },
+    };
+
+  return { isListening: isListening };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { LastFMHandler } from "./handlers/music";
 
-const app = new Hono()
+type Bindings = {
+  LASTFM_API_KEY: string;
+};
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{ Bindings: Bindings }>();
 
-export default app
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/api/music", async (c) => {
+  const LASTFM_API_KEY = c.env.LASTFM_API_KEY;
+
+  const json = await LastFMHandler(LASTFM_API_KEY);
+
+  return c.json(json, 200, {
+    "Content-Type": "application/json",
+  });
+});
+
+export default app;
